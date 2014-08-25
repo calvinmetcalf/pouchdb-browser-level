99d07dc90542d815a1da86bbc064e0ea78283ba7
Calvin Metcalf <cmetcalf@appgeo.com>
2014-08-25 15:27:25 -0400

    finish publish script

--- publish.sh
+++ publish.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+git checkout -b build
 VERSION=$1
 ./fetch.sh $VERSION
+git add lib package.json
+git commit -m 'build $1'
+git tag $1
+git push origin $1
+npm publish
+git checkout master
+git branch -D build
